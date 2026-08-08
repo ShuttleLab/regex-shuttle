ce73833f420cd72d20161b11621a7550e11bbc72
ShuttleLab <support@shuttlelab.org>
2026-08-08 13:29:11 +0800

    feat: add secretary.shuttlelab.org to sibling sites footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -27,6 +27,7 @@ const SIBLING_SITES: { name: string; host: string; featured?: boolean }[] = [
   { name: "Time", host: "time.shuttlelab.org" },
   { name: "SMS Forwarder", host: "smsforwarder.shuttlelab.org" },
   { name: "NetPulse", host: "netpulse.shuttlelab.org" },
+  { name: "Secretary", host: "secretary.shuttlelab.org" },
 ];
 
 const TOOL_PAGES: { href: string; key: string }[] = [
